2a212f38727e014caf749a9a218fa14750d99ae9
Mireli Borges <mika.oliveira1998@gmail.com>
2025-04-10 15:31:32 -0300

    Tema

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ import Home from './pages/home/Home'
 import Login from './pages/home/login/Login'
 import Cadastro from './pages/home/cadastro/Cadastro'
 import Footer from './components/navbar/footer/Footer'
+import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/navbar/temas/listatemas/ListaTemas'
+import FormTema from './components/navbar/temas/formtema/FormTema'
+import DeletarTema from './components/navbar/temas/deletartemas/DeletarTema'
 
 
 
 function App() {
   return (
     <>
+        <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -19,10 +24,16 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
+              
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
   )
 }
